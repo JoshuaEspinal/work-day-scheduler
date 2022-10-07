860342044a2bd91661b50f12b7562b68ffeb6dd6
KuuraKurta <joshuaespinal9401@gmail.com>
2022-10-06 23:56:38 -0400

    added loop through each item for business hours

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -14,4 +14,11 @@ $(document).ready(function () {
   // this is a 24 hour clock so 13 would equal 1 pm, 14 equals 2pm
   // this array is from 9 am to 5pm on a 24 clock after formatting with moment
   var businessHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  //   loop through moment business hours array because we want to dynamically create a time block
+  // for each item inside of this array
+  businessHours.forEach(function (hour, index) {
+    // here we are converting each hour to be am or pm -
+    // refer to moment docs to find this format (o so like..)
+    var formatBusinessHours = moment().hour(hour).format("h A");
+  });
 });
