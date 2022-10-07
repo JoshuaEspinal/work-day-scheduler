04c1f2e32f985595080485918f153150ac1782cb
KuuraKurta <joshuaespinal9401@gmail.com>
2022-10-06 23:52:46 -0400

    added array for businessHours

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -8,4 +8,10 @@ $(document).ready(function () {
 
   // this sets current date into p tag with id of currentDay
   $("#currentDay").text(todaysDate);
+  var container = $(".container");
+
+  // set an array of moment hours
+  // this is a 24 hour clock so 13 would equal 1 pm, 14 equals 2pm
+  // this array is from 9 am to 5pm on a 24 clock after formatting with moment
+  var businessHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 });
