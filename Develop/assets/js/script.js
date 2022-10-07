edbb8f111d8572201366a3a42f06f86837e01a62
KuuraKurta <joshuaespinal9401@gmail.com>
2022-10-06 23:50:03 -0400

    added date to jumbotron

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,36 +1,11 @@
-// execute when the DOM is loaded 
+// execute when the DOM is fully loaded.
 
 $(document).ready(function () {
+  // formats current day using moment to the desired format (Thursday, October 6th)
+  // documentation lives here for moment: https://momentjs.com/
 
-    var container = $('.container');
-
-    for (var i = 1; i < 9; i++) {
-        console.log(i);
-        container.append(`<div class="time-block">
-        <div class="row">
-          <div class="hour">
-            <span class="my-2 mx-2">9AM</span>
-          </div>
-          <textarea id="9AM" class="past"></textarea>
-          <button id="9AM-btn" class="saveBtn">
-            <i class="fa fa-regular fa-lock"></i>
-          </button>
-        </div>
-      </div>`)
-    }
-    // read from localstorage and set to textarea value
-    $("#9AM").val(localStorage.getItem("9AM") || "");
-    // formats current day using moment
   var todaysDate = moment().format("dddd, MMMM Do");
-
-  console.log(todaysDate);
 
   // this sets current date into p tag with id of currentDay
   $("#currentDay").text(todaysDate);
-
-  $("#9AM-btn").click(function () {
-    var nineAmTextArea = $("#9AM").val();
-    console.log(nineAmTextArea);
-    localStorage.setItem("9AM", nineAmTextArea);
-  });
 });
